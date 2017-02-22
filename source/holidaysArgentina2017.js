42bbdaba66a70107c8238baa968f6bff830575ba
fbridger <fbridger@gmail.com>
2017-02-22 11:58:17 -0300

    Fixing 2017 holidays in Argentina. Published version 1.0.7

--- source/holidaysArgentina2017.js
+++ source/holidaysArgentina2017.js
@@ -5,9 +5,8 @@ addHoliday("ar", 2017, 2,  28, "Carnaval");
 addHoliday("ar", 2017, 4, 13,  "Jueves Santo");
 addHoliday("ar", 2017, 4, 14,  "Viernes Santo");
 
-addHoliday("ar", 2017, 6, 17,  "Paso a la Inmortalidad del Gral. Don Martín Güemes", "nolaborable");
-addHoliday("ar", 2017, 6, 19,  "Feriado Puente Turistico", "puente");
+addHoliday("ar", 2017, 6, 17,  "Paso a la Inmortalidad del Gral. Don Martín Güemes");
 
-addHoliday("ar", 2017, 8, 17, "Paso a la Inmortalidad del General José de San Martín", "trasladable", 14);
-addHoliday("ar", 2017, 10,12, "Día del Respeto a la Diversidad Cultural", "trasladable", 9);
-addHoliday("ar", 2017, 11,20, "Día de la Soberanía Nacional",	"trasladable", 27);
+addHoliday("ar", 2017, 8, 17, "Paso a la Inmortalidad del General José de San Martín", "trasladable", 21);
+addHoliday("ar", 2017, 10,12, "Día del Respeto a la Diversidad Cultural", "trasladable", 16);
+addHoliday("ar", 2017, 11,20, "Día de la Soberanía Nacional");
